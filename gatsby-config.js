f8cdfd9e77b5e21f7a25d261c4567e96c4b322df
Louis CHAN <drastrov@msn.com>
2023-02-25 13:14:12 -0500

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Navon apps homepage`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-google-gtag"]
+};
